1f0667393db48ce5c60d0ef89ae3c4102edaa7c5
M. Atakan Gurkan <ato.gurkan@gmail.com>
2023-02-13 13:38:35 +0300

    do instead of loop

--- prob1.lisp
+++ prob1.lisp
@@ -3,7 +3,7 @@
   (/ (loop for i from 0 below nN
 	   sum (make-random-sample)) nN)) 
 
-(defun make-random-sample ()
+(defun make-random-sample-old ()
   "creates random numbers in range [0, 1) until their sum exceeds 1.0.
    returns the number of numbers created.
    It feels like this should be done with do, rather than loop."
@@ -12,4 +12,10 @@
       (incf Nsum (random 1.0))
       (incf n)
       (when (> Nsum 1.0) (return n)))))
+
+(defun make-random-sample ()
+  "same as above, but this is more compact."
+  (do ((n 0 (1+ n))
+       (Nsum 0 (+ (random 1.0) Nsum)))
+      ((> Nsum 1.0) n)))
     
